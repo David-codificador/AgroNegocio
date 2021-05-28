a468ddc46703df802d1c2db67bd63131bfa64491
David-codificador <natan_seabra@hotmail.com>
2021-05-27 21:53:50 -0300

    Cor do Site Inicial

--- App/Views/layoutsSite/menu.php
+++ App/Views/layoutsSite/menu.php
@@ -3,8 +3,8 @@
     <div class="topbar-one">
         <div class="container">
             <div class="topbar-one__left">
-                <a href="mailto:needhelp@tripo.com"><span class="icon-message"></span>needhelp@agrikol.com</a>
-                <a href="tel:666-999-0000"><span class="icon-phone-call"></span>666 888 0000</a>
+                <a href="mailto:needhelp@tripo.com"><span class="icon-message"></span>natan_seabra@hotmail.com</a>
+                <a href="tel:666-999-0000"><span class="icon-phone-call"></span>(062) 9 9813 - 1151</a>
             </div>
             <div class="topbar-one__middle">
                 <a href="index.html" class="main-nav__logo">
@@ -21,43 +21,67 @@
         </div>
     </div>
 
-    <header class="main-nav__header-one corpo">
-        <nav class="header-navigation stricky corpo">
-            <div class="container clearfix">
-                <!-- Brand and toggle get grouped for better mobile display -->
-                <div class="main-nav__left">
+    <header class="main-nav__header_two clearfix corpo">
+        <nav class="header-navigation stricky clearfix">
+            <div class="container">
+                <div class="main-nav__header_two_content">
+                    <div class="mobile_menu_icon_two">
+                        <a href="#" class="side-menu__toggler">
+                            <i class="fa fa-bars"></i>
+                        </a>
+                    </div>
+                    <div class="main-nav__main-navigation two float-left">
+                        <ul class="main-nav__navigation-box">
+                            <li>
+                                <a href="<?= LINK ?>">Início</a>
+                            </li>
+                            <li>
+                                <a href="<?= LINK ?>sobre">Sobre</a>
+                            </li>
+                            <li class="dropdown">
+                                <a href="#">Produtos</a>
+                                <ul class="cor_menu_link">
+                                    <li><a href="news.html">Melancia Style</a></li>
+                                    <li><a href="news_detail.html">Melancia Manchester</a></li>
+                                </ul><!-- /.sub-menu -->
+                            </li> 
+                            <li class="dropdown">
+                                <a href="#">Pages</a>
+                                <ul>
+                                    <li><a href="about.html">About</a></li>
+                                    <li><a href="why_choose_us.html">Why Choose Us</a></li>
+                                    <li><a href="farmers.html">Farmers</a></li>
+                                    <li><a href="gallery.html">Gallery</a></li>
+                                </ul><!-- /.sub-menu -->
+                            </li>
+                            <li class="dropdown">
+                                <a href="#">News</a>
+                                <ul>
+                                    <li><a href="news.html">News</a></li>
+                                    <li><a href="news_detail.html">News Details</a></li>
+                                </ul><!-- /.sub-menu -->
+                            </li>
+                            <li>
+                                <a href="contact.html">Contact</a>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div class="main_nav_right_two float-right">
+                        <div class="icon_search_box">
+                            <a href="#" class="main-nav__search search-popup__toggler"><i
+                                    class="icon-magnifying-glass"></i></a>
+                        </div>
+                        <div class="icon_cart_box">
+                            <a href="cart.html">
+                                <span class="icon-shopping-cart"></span>
+                            </a>
+                        </div>
+                    </div>
 
                 </div>
-                <div class="main-nav__main-navigation">
-                    <ul class=" main-nav__navigation-box">
-                        <li>
-                            <a href="<?= LINK ?>">Início</a>
-                        </li>
-                        <li>
-                            <a href="<?= LINK ?>sobre">Sobre</a>
-                        </li>
-                        <li class="dropdown">
-                            <a href="#">Produtos</a>
-                            <ul cor_menu_link>
-                                <li><a href="news.html">Melancia Style</a></li>
-                                <li><a href="news_detail.html">Melancia Manchester</a></li>
-                            </ul><!-- /.sub-menu -->
-                        </li> 
-                        <li>
-                            <a href="<?= LINK ?>curiosidades">Curiosidades</a>
-                        </li>
-                        <li>
-                            <a href="<?= LINK ?>galeria">Galeria de Fotos</a>
-                        </li>
-                        <li>
-                            <a href="<?= LINK ?>safra">Notícias</a>
-                        </li>                    
-                        <li>
-                            <a href="contact.html">Contato</a>
-                        </li>
-                    </ul>
-                </div><!-- /.navbar-collapse -->                              
             </div>
+
         </nav>
     </header>
 </div>
